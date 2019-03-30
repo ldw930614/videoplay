34bb936ce582e083c95f2770a8bac6c89905b66d
davy614 <276727139@qq.com>
2019-03-30 18:08:42 +0800

    navigation

--- app/src/main/java/com/acdia/videoplayldw/MainActivity.java
+++ app/src/main/java/com/acdia/videoplayldw/MainActivity.java
@@ -2,12 +2,76 @@ package com.acdia.videoplayldw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.acdia.videoplayldw.view.BTFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private BTFragment btFragment;
+
+    @BindView(R.id.main)
+    TextView main;
+    @BindView(R.id.localvideo)
+    TextView localvideo;
+    @BindView(R.id.aboutmine)
+    TextView aboutmine;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        main.setOnClickListener(this);
+        localvideo.setOnClickListener(this);
+        aboutmine.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch(view.getId())
+        {
+            case R.id.main:
+                togglefrag(0);
+                break;
+            case R.id.localvideo:
+                togglefrag(1);
+                break;
+            case R.id.aboutmine:
+                togglefrag(2);
+                break;
+        }
+    }
+
+    public void togglefrag(int pageNum)
+    {
+        switch (pageNum)
+        {
+            case 0:
+                main.setSelected(true);
+                localvideo.setSelected(false);
+                aboutmine.setSelected(false);
+                break;
+            case 1:
+                main.setSelected(false);
+                localvideo.setSelected(true);
+                aboutmine.setSelected(false);
+                break;
+            case 2:
+                main.setSelected(false);
+                localvideo.setSelected(false);
+                aboutmine.setSelected(true);
+                break;
+            default:
+
+                break;
+        }
     }
 }
