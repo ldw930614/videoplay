2b30be0e25e00c2a4b882b4dfb8a312d4ad12981
davy614 <276727139@qq.com>
2019-03-30 11:48:16 +0800

    splashactivity

--- app/src/main/java/com/acdia/videoplayldw/splash/SplashActivity.java
+++ app/src/main/java/com/acdia/videoplayldw/splash/SplashActivity.java
@@ -1,18 +1,44 @@
 package com.acdia.videoplayldw.splash;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.acdia.videoplayldw.MainActivity;
 import com.acdia.videoplayldw.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY_TIME = 2000;//毫秒
+    private static final int ISOK = 1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
-
+        mHandler.sendEmptyMessageDelayed(ISOK,SPLASH_DELAY_TIME);
     }
+
+    private Handler mHandler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            switch (msg.what)
+            {
+                case 1000:
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    };
 }
