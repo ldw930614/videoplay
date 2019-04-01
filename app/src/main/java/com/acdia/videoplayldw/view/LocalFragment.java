cc4ae677542f4df25a26b499103ca87ed70528bd
davy614 <276727139@qq.com>
2019-04-01 23:05:19 +0800

    toolbar

--- app/src/main/java/com/acdia/videoplayldw/view/LocalFragment.java
+++ app/src/main/java/com/acdia/videoplayldw/view/LocalFragment.java
@@ -2,20 +2,27 @@ package com.acdia.videoplayldw.view;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.acdia.videoplayldw.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
 
 public class LocalFragment extends Fragment {
 
+    @BindView(R.id.toobar)
+    Toolbar toobar;
 
     Unbinder unbinder;
+
+
 
     public static LocalFragment localFragment;
 
@@ -45,6 +52,12 @@ public class LocalFragment extends Fragment {
     private void initView()
     {
 
+        toobar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"aaa",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
